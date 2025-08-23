b0382e57050d5a9d95ae497898d176a090a67bca
Team-Riz <avroperations87@gmail.com>
2025-08-23 12:18:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // ------------------------
+// FLY ANIMATION
+// ------------------------
+function animateFly(id) {
+    let icon = document.getElementById(id);
+    icon.style.opacity = 1;
+    icon.style.transform = 'translate(0, -50px)';
+    setTimeout(() => {
+        icon.style.opacity = 0;
+        icon.style.transform = 'translate(0,0)';
+    }, 800);
+}
+
+// ------------------------
 // SEARCH FUNCTION
 // ------------------------
 function searchConverter() {
@@ -15,81 +28,59 @@ function searchConverter() {
 }
 
 // ------------------------
-// LENGTH CONVERTER
+// LENGTH
 // ------------------------
-const lengthRates = {
-    meter: 1,
-    kilometer: 1000,
-    centimeter: 0.01,
-    mile: 1609.34,
-    foot: 0.3048,
-    inch: 0.0254
-};
+const lengthRates = { meter:1, kilometer:1000, centimeter:0.01, mile:1609.34, foot:0.3048, inch:0.0254 };
 function convertLength() {
     let val = parseFloat(document.getElementById('lengthValue').value);
     let from = document.getElementById('lengthFrom').value;
     let to = document.getElementById('lengthTo').value;
-    let result = val * lengthRates[from] / lengthRates[to];
-    document.getElementById('lengthResult').innerText = result.toFixed(2) + ' ' + to;
+    let result = val * lengthRates[from]/lengthRates[to];
+    document.getElementById('lengthResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('lengthFly');
 }
 
 // ------------------------
-// WEIGHT CONVERTER
+// WEIGHT
 // ------------------------
-const weightRates = {
-    kilogram: 1,
-    gram: 0.001,
-    pound: 0.453592,
-    ounce: 0.0283495
-};
+const weightRates = { kilogram:1, gram:0.001, pound:0.453592, ounce:0.0283495 };
 function convertWeight() {
     let val = parseFloat(document.getElementById('weightValue').value);
     let from = document.getElementById('weightFrom').value;
     let to = document.getElementById('weightTo').value;
-    let result = val * weightRates[from] / weightRates[to];
-    document.getElementById('weightResult').innerText = result.toFixed(2) + ' ' + to;
+    let result = val * weightRates[from]/weightRates[to];
+    document.getElementById('weightResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('weightFly');
 }
 
 // ------------------------
-// TEMPERATURE CONVERTER
+// TEMPERATURE
 // ------------------------
 function convertTemperature() {
     let val = parseFloat(document.getElementById('tempValue').value);
     let from = document.getElementById('tempFrom').value;
     let to = document.getElementById('tempTo').value;
-    let celsius;
-
-    if(from==='celsius') celsius = val;
-    else if(from==='fahrenheit') celsius = (val-32)*5/9;
-    else if(from==='kelvin') celsius = val-273.15;
-
-    let result;
-    if(to==='celsius') result = celsius;
-    else if(to==='fahrenheit') result = celsius*9/5+32;
-    else if(to==='kelvin') result = celsius+273.15;
-
-    document.getElementById('tempResult').innerText = result.toFixed(2) + ' ' + to;
+    let celsius = (from==='celsius')?val:(from==='fahrenheit')?(val-32)*5/9:val-273.15;
+    let result = (to==='celsius')?celsius:(to==='fahrenheit')?celsius*9/5+32:celsius+273.15;
+    document.getElementById('tempResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('tempFly');
 }
 
 // ------------------------
-// VOLUME CONVERTER
+// VOLUME
 // ------------------------
-const volumeRates = {
-    liter:1,
-    milliliter:0.001,
-    gallon:3.78541,
-    cup:0.24
-};
+const volumeRates = { liter:1, milliliter:0.001, gallon:3.78541, cup:0.24 };
 function convertVolume() {
     let val = parseFloat(document.getElementById('volumeValue').value);
     let from = document.getElementById('volumeFrom').value;
     let to = document.getElementById('volumeTo').value;
-    let result = val*volumeRates[from]/volumeRates[to];
+    let result = val * volumeRates[from]/volumeRates[to];
     document.getElementById('volumeResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('volumeFly');
 }
 
 // ------------------------
-// SPEED CONVERTER
+// SPEED
 // ------------------------
 const speedRates = { kmh:1, mph:1.60934, ms:3.6 };
 function convertSpeed() {
@@ -98,10 +89,11 @@ function convertSpeed() {
     let to = document.getElementById('speedTo').value;
     let result = val*speedRates[from]/speedRates[to];
     document.getElementById('speedResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('speedFly');
 }
 
 // ------------------------
-// AREA CONVERTER
+// AREA
 // ------------------------
 const areaRates = { sqmeter:1, sqkilometer:1e6, sqmile:2.59e6, sqyard:0.836127 };
 function convertArea() {
@@ -110,10 +102,11 @@ function convertArea() {
     let to = document.getElementById('areaTo').value;
     let result = val*areaRates[from]/areaRates[to];
     document.getElementById('areaResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('areaFly');
 }
 
 // ------------------------
-// TIME CONVERTER
+// TIME
 // ------------------------
 const timeRates = { second:1, minute:60, hour:3600 };
 function convertTime() {
@@ -122,10 +115,11 @@ function convertTime() {
     let to = document.getElementById('timeTo').value;
     let result = val*timeRates[from]/timeRates[to];
     document.getElementById('timeResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('timeFly');
 }
 
 // ------------------------
-// CURRENCY CONVERTER (Static rates example)
+// CURRENCY (static example)
 // ------------------------
 const currencyRates = { USD:1, QAR:3.64, PKR:278, EUR:0.93 };
 function convertCurrency() {
@@ -134,4 +128,5 @@ function convertCurrency() {
     let to = document.getElementById('currencyTo').value;
     let result = val*currencyRates[from]/currencyRates[to];
     document.getElementById('currencyResult').innerText = result.toFixed(2)+' '+to;
+    animateFly('currencyFly');
 }
